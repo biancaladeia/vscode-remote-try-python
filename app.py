3f73c8a9a15165bc2bbbf9ec8dc4b867f8dd8a6d
Bianca Ladeia <138152320+biancaladeia@users.noreply.github.com>
2023-09-20 20:26:25 +0000

    pedra papel e tesoura

--- app.py
+++ app.py
@@ -1,11 +1,60 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+#importing the random module
+import random
 
-from flask import Flask
-app = Flask(__name__)
+options = ['pedra', 'papel', 'tesoura'] #creating a list of options
+score = 0 #initializing the score to 0
+rounds_played = 0 #initializing the rounds played to 0
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+while True:
+    random_choice = random.choice(options) # computador escolhe aleatoriamente
+    player_choice = input("Escolha pedra, papel ou tesoura: ") # jogador escolhe
+
+    #se o meu jogador escolher pedra
+    if player_choice == 'pedra':
+        rounds_played += 1
+
+        if random_choice == 'pedra':
+            print("Empate!")
+        elif random_choice == 'papel':
+            print("Você perdeu!")
+        else:
+            print("Você ganhou!")
+            score += 1
+
+    #se o meu jogador escolher papel
+    elif player_choice == 'papel':
+        rounds_played += 1
+
+        if random_choice == 'papel':
+            print("Empate!")
+        elif random_choice == 'tesoura':
+            print("Você perdeu!")
+        else:
+            print("Você ganhou!")
+            score += 1
+
+    #se o meu jogador escolher tesoura
+    elif player_choice == 'tesoura':
+        rounds_played += 1
+        
+        if random_choice == 'tesoura':
+            print("Empate!")
+        elif random_choice == 'pedra':
+            print("Você perdeu!")
+        else:
+            print("Você ganhou!")
+            score += 1
+    
+    #se o meu jogador escolher algo diferente de pedra papel e tesoura
+    else:
+        play_again = input("Quer jogar novamente? (s/n) ")
+        if play_again == 's':
+            continue
+        elif play_again == 'n':
+            print("Você ganhou", score, "vezes!")
+            break
+
+        else:
+            print("Opção inválida!")
+            break
+
